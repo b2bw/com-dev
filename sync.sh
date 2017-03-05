a6a98efc218dc86bf4a9557307ea11b60538f568
Forestry.io <support@forestry.io>
2017-03-05 23:12:18 +0000

    Deploy from Forestry.io

--- sync.sh
+++ sync.sh
@@ -10,11 +10,11 @@ wget -kO $tmp https://b2bw.github.io/com-dev/
 
 # upload temp file to secondary hosting via s3
 aws s3 cp --acl public-read $tmp \
-    $prefix/de/projekte/entwicklungszusammenarbeit.htm
+    $prefix/de/nachhaltige-entwicklung.htm
 
 # remove temp file
 rm $tmp
 
 # send an email to local user root
-echo "http://www.bos-schweiz.ch/de/projekte/entwicklungszusammenarbeit.htm" \
-    | mail -s "ComDev has been updated." root
+echo "http://www.bos-schweiz.ch/de/nachhaltige-entwicklung.htm" \
+    | mail -s "LP ComDev has been updated." root
