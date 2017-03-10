5d09289ae13eb25de0bf265c1735786ad20cb63b
Forestry.io <support@forestry.io>
2017-03-10 20:37:29 +0000

    Deploy from Forestry.io

--- sync.sh
+++ sync.sh
@@ -1,23 +1,68 @@
-#!/bin/sh
+#!/bin/bash
 
 prefix=s3://www.bos-schweiz.ch
 
 # create a temp file to hold html
 tmp=$(mktemp --suffix=.html)
 
-# mapping
-# https://b2bw.github.io/com-dev/ -> /de/nachhaltige-entwicklung.htm
-# https://b2bw.github.io/com-dev/bildungsprojekte.html -> /de/bildungsprojekte.htm
-# https://b2bw.github.io/com-dev/gesundheit.html -> /de/gesundheit.htm
-# https://b2bw.github.io/com-dev/mawas.html -> /de/mawas.htm
-# https://b2bw.github.io/com-dev/mikrokredit.html -> /de/mikrokredit.htm
+
+# MAPPING
+# -------
+# http://born2bewild.org/com-dev/                      -> /de/nachhaltige-entwicklung.htm
+# http://born2bewild.org/com-dev/bildungsprojekte.html -> /de/nachhaltige-entwicklung/bildungsprojekte.htm
+# http://born2bewild.org/com-dev/gesundheit.html       -> /de/nachhaltige-entwicklung/gesundheit.htm
+# http://born2bewild.org/com-dev/mawas.html            -> /de/nachhaltige-entwicklung/mawas.htm
+# http://born2bewild.org/com-dev/mikrokredit.html      -> /de/nachhaltige-entwicklung/mikrokredit.htm
+
+
+base='http://born2bewild.org/com-dev'
+target='/de/nachhaltige-entwicklung'
 
 # retrieve html from primary hosting into temp file
-wget -kO $tmp https://b2bw.github.io/com-dev/
+wget -kO $tmp $base/
+
+# convert specific links
+sed -i "s|$base/bildungsprojekte.html|$target/bildungsprojekte.htm|" $tmp
+sed -i "s|$base/gesundheit.html|$target/gesundheit.htm|" $tmp
+sed -i "s|$base/mawas.html|$target/mawas.htm|" $tmp
+sed -i "s|$base/mikrokredit.html|$target/mikrokredit.htm|" $tmp
 
 # upload temp file to secondary hosting via s3
-aws s3 cp --acl public-read $tmp \
-    $prefix/de/nachhaltige-entwicklung.htm
+aws s3 cp --acl public-read $tmp $prefix$target.htm
+
+
+# bildungsprojekte
+wget -kO $tmp $base/bildungsprojekte.html
+sed -i "s|$base/gesundheit.html|$target/gesundheit.htm|" $tmp
+sed -i "s|$base/mawas.html|$target/mawas.htm|" $tmp
+sed -i "s|$base/mikrokredit.html|$target/mikrokredit.htm|" $tmp
+sed -i "s|$base/|$target.htm|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/bildungsprojekte.htm
+
+# gesundheit
+wget -kO $tmp $base/gesundheit.html
+sed -i "s|$base/bildungsprojekte.html|$target/bildungsprojekte.htm|" $tmp
+sed -i "s|$base/mawas.html|$target/mawas.htm|" $tmp
+sed -i "s|$base/mikrokredit.html|$target/mikrokredit.htm|" $tmp
+sed -i "s|$base/|$target.htm|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/gesundheit.htm
+
+# mawas
+wget -kO $tmp $base/mawas.html
+sed -i "s|$base/bildungsprojekte.html|$target/bildungsprojekte.htm|" $tmp
+sed -i "s|$base/gesundheit.html|$target/gesundheit.htm|" $tmp
+sed -i "s|$base/mikrokredit.html|$target/mikrokredit.htm|" $tmp
+sed -i "s|$base/|$target.htm|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/mawas.htm
+
+# mikrokredit
+wget -kO $tmp $base/mikrokredit.html
+sed -i "s|$base/bildungsprojekte.html|$target/bildungsprojekte.htm|" $tmp
+sed -i "s|$base/gesundheit.html|$target/gesundheit.htm|" $tmp
+sed -i "s|$base/mawas.html|$target/mawas.htm|" $tmp
+sed -i "s|$base/|$target.htm|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/mikrokredit.htm
+
 
 # remove temp file
 rm $tmp
